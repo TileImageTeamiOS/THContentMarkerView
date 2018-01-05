4728bbf813a93bd0666b5b32183261fb0df1ee7c
jw <hjw01234@gmail.com>
2018-01-05 14:17:09 +0900

    video editor

--- Pods/Target Support Files/Pods-ScrollViewContent/Pods-ScrollViewContent-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ScrollViewContent : NSObject
-@end
-@implementation PodsDummy_Pods_ScrollViewContent
-@end
--- ScrollViewContent/EditorViewController.swift
+++ ScrollViewContent/EditorViewController.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
-import FileExplorer
 
 class EditorViewController: UIViewController {
+    
+    var imagePicker: UIImagePickerController!
+    
     @IBOutlet weak var audioTitle: UILabel!
-    let fileExplorer = FileExplorerViewController()
-    var audio = true
+    
     @IBAction func backButtonAction(_ sender: Any) {
         self.navigationController?.popToRootViewController(animated: true)
     }
+    
     @IBOutlet weak var videoTitle: UILabel!
     
     @IBAction func doneButtonAction(_ sender: Any) {
@@ -24,40 +26,36 @@ class EditorViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        fileExplorer.canChooseFiles = true //specify whether user is allowed to choose files
-        fileExplorer.canChooseDirectories = false //specify whether user is allowed to choose directories
-        fileExplorer.delegate = self as? FileExplorerViewControllerDelegate
-
-        // Do any additional setup after loading the view.
-    }
-    public func fileExplorerViewController(_ controller: FileExplorerViewController, didChooseURLs urls: [URL]) {
-        print(urls)
     }
     @IBAction func chooseAudio(_ sender: Any) {
-        audio = true
-        fileExplorer.fileFilters = [Filter.extension("mp3")]
-        self.present(fileExplorer, animated: true, completion: nil)
     }
     @IBAction func chooseVideo(_ sender: Any) {
-        audio = false
-        fileExplorer.fileFilters = [Filter.extension("mp4"), Filter.extension("avi")]
-        self.present(fileExplorer, animated: true, completion: nil)
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = false
+        imagePicker.mediaTypes = ["public.movie"]
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+   
+}
+
+extension EditorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let videoPath: NSURL = info["UIImagePickerControllerReferenceURL"] as! NSURL
+        
+        videoTitle.text = videoPath.lastPathComponent
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        imagePicker = nil
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imagePicker = nil
     }
-    */
-
 }
