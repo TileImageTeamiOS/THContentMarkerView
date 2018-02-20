66d063cc5b326c70a7c0482e3ab262d78e863b5f
Seong ho Hong <grohong76@gmail.com>
2018-02-20 23:30:51 +0900

    set test title content view

--- THContentMarkerViewUnitTests/THTitleContentViewUnitTests.swift
+++ THContentMarkerViewUnitTests/THTitleContentViewUnitTests.swift
@@ -7,29 +7,33 @@
 //
 
 import XCTest
+@testable import THContentMarkerView
 
 class THTitleContentViewUnitTests: XCTestCase {
-    
+    let thTitleContent = THTitleContentView()
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        thTitleContent.setView()
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testSetView() {
+        XCTAssertNotNil(thTitleContent.delegate)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testSetContent() {
+        // test set title content
+        thTitleContent.delegate.setContent(info: "test title")
+        XCTAssertNotEqual(thTitleContent.titleLabel.frame.size, CGSize.zero)
+        XCTAssertEqual(thTitleContent.titleLabel.text, "test title")
+
+        // test dismiss title content
+        thTitleContent.delegate.dismiss()
+        XCTAssertEqual(thTitleContent.titleLabel.frame.size, CGSize.zero)
+        XCTAssertEqual(thTitleContent.titleLabel.text, "")
     }
-    
 }
